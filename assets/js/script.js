d818b2e44ef3158fd62ea2c330e685a01a28ce91
akash <nandoliyaakash990@gmail.com>
2025-03-12 10:29:33 +0530

    inspect enable

--- assets/js/script.js
+++ assets/js/script.js
@@ -176,9 +176,9 @@ document.onkeydown = function (e) {
     if (e.keyCode == 123) {
         return false;
     }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
+    // if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    //     return false;
+    // }
     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
         return false;
     }
